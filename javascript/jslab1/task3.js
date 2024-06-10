42d28c83e8b1dd7dbcc9cf61efcaa942e2aa1df0
Ivan44k <91094749+Ivan44k@users.noreply.github.com>
2024-06-10 13:27:17 +0300

    Update task3.js

--- javascript/jslab1/task3.js
+++ javascript/jslab1/task3.js
@@ -1,16 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Document</title>
-</head>
-<body>
-<scrфipt>
-'use strict'
-var a = prompt("Введите число больше 100: ")
-while((isNaN(+a) || +a <= 100) && a!=null)
-var a = prompt("Введите число больше 100")
-</script>
-</body>
-</html>
+
